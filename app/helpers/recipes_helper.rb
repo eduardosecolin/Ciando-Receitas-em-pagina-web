80f21ad5071899a1eb315b5eec10fdbcaab0af2d
Eduardo Secolin <dusecolin@yahoo.com.br>
2017-12-14 10:46:49 -0200

    Criando helpers

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,12 @@
 module RecipesHelper
+
+   #metodo qur formata a caloria
+   def formata_caloria(recipe)
+    if recipe.light?
+        content_tag(:strong , "Light (menos de 100 calorias)")
+    else    
+      recipe.calories
+    end
+   end
+
 end
