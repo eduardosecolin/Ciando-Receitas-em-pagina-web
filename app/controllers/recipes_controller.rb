81a03fe215cfc59ef96709c0952b1b70dcc221b9
Eduardo Secolin <dusecolin@yahoo.com.br>
2017-12-15 15:43:29 -0200

    Criando a form de edição

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,26 @@
 class RecipesController < ApplicationController
-   
+   # os metodos criados são acitions
+
+   # metodo que retorna o indice das receitas
     def index
       @recipes = Recipe.all
     end
-
+   
+   # metodo que mostra a receita em outra janela através de um link
     def show
       @recipe = Recipe.find(params[:id])
     end
+   
+   # metodo para editar um a receita através de um link 
+    def edit
+      @recipe = Recipe.find(params[:id])
+    end
+
+   # metodo para atualizar a recita editada
+   def update
+      @recipe = Recipe.find(params[:id])
+      recipe_param = params.require(:recipe).permit(:name, :stuff, :calories, :prepare_mode, :cost)
+      @recipe.update(recipe_param)
+      redirect_to @recipe
+   end
 end
